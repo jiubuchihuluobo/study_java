5a7082a408ea5b7c03c5dfcbafb0fad47335ce9a
jiubuchihuluobo <date20150619@icloud.com>
2022-11-26 23:41:49 +0800

    遍历数组

---
+++ day01/src/com/apple/www/ArrayTest.java
@@ -0,0 +1,29 @@
+package com.apple.www;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 4, 4};
+        int a = 0;
+
+        while (a < array.length) {
+            System.out.println(array[a]);
+            a += 1;
+        }
+        a = 0;
+
+        do {
+            System.out.println(array[a]);
+            a++;
+        } while (a < array.length);
+
+        for (a = 0; a < array.length; a++) {
+            System.out.println(array[a]);
+        }
+
+        int count = 0;
+        for (int i : array) {
+            count += i;
+        }
+        System.out.println(count);
+    }
+}
--- day01/src/com/apple/www/RandomInt.java
+++ day01/src/com/apple/www/RandomInt.java
@@ -1,7 +1,11 @@
 package com.apple.www;
 
+import java.util.Random;
+
 public class RandomInt {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        int num = random.nextInt(100) + 1;
+        System.out.println(num);
     }
 }
