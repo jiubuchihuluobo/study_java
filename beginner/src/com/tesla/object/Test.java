ea4e936a8067db72497f44956e2a821223483bbb
jiubuchihuluobo <date20150619@icloud.com>
2022-12-08 18:03:54 +0800

    添加测试文件

---
+++ beginner/src/com/tesla/object/Test.java
@@ -0,0 +1,11 @@
+package com.tesla.object;
+
+import com.tesla.people.Customer;
+
+public class Test {
+    public static void main(String[] args) {
+        Customer c1 = new Customer();
+        new Customer().toString();
+    }
+
+}
